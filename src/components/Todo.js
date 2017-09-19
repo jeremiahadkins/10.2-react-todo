9e3e44525634a6c0c259e35eef2fca11c241adde
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-09-19 12:08:40 -0400

    edit capability to todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,7 +9,9 @@ class Todo extends Component {
       todos: [
         {title: 'Test Todo'},
         {title: 'Another Todo'}
-      ]
+      ],
+      isEdit: false,
+      todoToEdit: null
     };
   }
 
@@ -25,11 +27,37 @@ class Todo extends Component {
     this.setState({todos: todos});
   }
 
+  updateTodo = (index, todo) => {
+    let todos = this.state.todos;
+    todos.splice(index, 1, todo);
+    this.setState({todos: todos});
+
+    this.hideEditMode();
+  }
+
+  hideEditMode = () => {
+    this.setState({isEdit: false, todoToEdit: null});
+  }
+
+  showEditMode = (todoIndex) => {
+    this.setState({isEdit: true, todoToEdit: todoIndex});
+  }
+
   render() {
     return (
       <div>
-        <TodoForm addTodo={this.addTodo}/>
-        <TodoList todos={this.state.todos} removeTodo={this.removeTodo}/>
+        <TodoForm 
+          isEdit={this.state.isEdit}
+          todoToEdit={this.state.todoToEdit}
+          addTodo={this.addTodo}
+          updateTodo={this.updateTodo}
+          todos={this.state.todos}
+        />
+        <TodoList
+          todos={this.state.todos}
+          removeTodo={this.removeTodo}
+          showEditMode={this.showEditMode}
+        />
       </div>
     );
   }
@@ -45,6 +73,15 @@ class TodoForm extends Component{
     this.state = {todoTitle: ''};
   }
 
+  // what is this?
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isEdit) {
+      console.log(nextProps.todos);
+      let todo = nextProps.todos[nextProps.todoToEdit];
+      this.setState({todoTitle: todo.title});
+    }
+  }
+
   handleChange = (e) => {
     // track todo title to the state
     this.setState({todoTitle: e.target.value});
@@ -52,8 +89,13 @@ class TodoForm extends Component{
 
   hanldeSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodo({title: this.state.todoTitle});
-    console.log(this.state.todoTitle);
+    
+    if (this.props.isEdit) {
+      this.props.updateTodo(this.props.todoToEdit, {title: this.state.todoTitle});
+    } else {
+      this.props.addTodo({title: this.state.todoTitle});
+    }
+    // console.log(this.state.todoTitle);
     this.setState({todoTitle: ''});
   }
 
@@ -61,7 +103,7 @@ class TodoForm extends Component{
     return (
       <form onSubmit={this.hanldeSubmit}>
         <input onChange={this.handleChange} type="text" name="todo-title" placeholder="Todo Title" value={this.state.todoTitle}/>
-        <input type="submit" className="btn btn-primary btn-sm" value="Add Todo" />
+        <input type="submit" className="btn btn-primary btn-sm" value={this.props.isEdit ? "Edit" : "Add"} />
       </form>
     );
   }
@@ -77,6 +119,7 @@ class TodoList extends Component{
       return (
         <li className="list-group-item" key={todo.title}>
           {todo.title} <a className="btn btn-danger btn-sm text-white" onClick={this.props.removeTodo.bind(this, index)}>Delete</a>
+          <a className="btn btn-sm btn-outline-secondary" onClick={(e) => {e.preventDefault(); this.props.showEditMode(index); }}>Edit</a>
         </li>
       );
     });
